privacy-scaling-explorations/snark-verifier

feat: change yul code into Solidity assembly (#47)
* feat: change yul code into Solidity assembly

Just changes to wrapping yul in solidity assembly block

* chore: loosen pragma

* chore: remove example sol file

snark-verifier-sdk/src/evm.rs
@@ -22,7 +22,7 @@ use itertools::Itertools;
 use rand::{rngs::StdRng, SeedableRng};
 pub use snark_verifier::loader::evm::encode_calldata;
 use snark_verifier::{
-    loader::evm::{compile_yul, deploy_and_call, EvmLoader},
+    loader::evm::{compile_solidity, deploy_and_call, EvmLoader},
     pcs::{
         kzg::{KzgAccumulator, KzgAsVerifyingKey, KzgDecidingKey, KzgSuccinctVerifyingKey},
         AccumulationDecider, AccumulationScheme, PolynomialCommitmentScheme,
@@ -148,13 +148,13 @@ where
         PlonkVerifier::<AS>::read_proof(&dk, &protocol, &instances, &mut transcript).unwrap();
     PlonkVerifier::<AS>::verify(&dk, &protocol, &instances, &proof).unwrap();
 
-    let yul_code = loader.yul_code();
-    let byte_code = compile_yul(&yul_code);
+    let sol_code = loader.solidity_code();
+    let byte_code = compile_solidity(&sol_code);
     if let Some(path) = path {
         path.parent()
             .and_then(|dir| fs::create_dir_all(dir).ok())
             .unwrap();
-        fs::write(path, yul_code).unwrap();
+        fs::write(path, sol_code).unwrap();
     }
     byte_code
 }

yanbowen1994/scroll-snark-verifier

snark-verifier-sdk/src/evm.rs
@@ -21,7 +21,7 @@ use itertools::Itertools;
 use rand::Rng;
 pub use snark_verifier::loader::evm::encode_calldata;
 use snark_verifier::{
-    loader::evm::{compile_yul, EvmLoader, ExecutorBuilder},
+    loader::evm::{compile_solidity, ExecutorBuilder, EvmLoader},
     pcs::{
         kzg::{Bdfg21, Gwc19, Kzg, KzgAccumulator, KzgDecidingKey, KzgSuccinctVerifyingKey},
         Decider, MultiOpenScheme, PolynomialCommitmentScheme,
@@ -149,11 +149,11 @@ where
     let proof = Plonk::<PCS>::read_proof(&svk, &protocol, &instances, &mut transcript);
     Plonk::<PCS>::verify(&svk, &dk, &protocol, &instances, &proof);
 
-    let yul_code = loader.yul_code();
-    let byte_code = compile_yul(&yul_code);
+    let sol_code = loader.solidity_code();
+    let byte_code = compile_solidity(&sol_code);
     if let Some(path) = path {
         path.parent().and_then(|dir| fs::create_dir_all(dir).ok()).unwrap();
-        fs::write(path, yul_code).unwrap();
+        fs::write(path, sol_code).unwrap();
     }
     byte_code
 }